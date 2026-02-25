b761123e0dfa3f4bb0975f53377afeb5aa901194
fyroz118 <163815659+fyroz118@users.noreply.github.com>
2026-02-25 21:41:11 +0600

    Update import_questions.py

--- import_questions.py
+++ import_questions.py
@@ -1,30 +1,37 @@
-import csv
-from database import cursor, conn
+def load_questions():
+    cursor.execute("SELECT COUNT(*) FROM questions")
+    count = cursor.fetchone()[0]
 
-with open("questions_usmle.csv", newline="", encoding="utf-8") as file:
-    reader = csv.DictReader(file)
-    
-    for row in reader:
-        cursor.execute("""
-        INSERT INTO questions(
-            exam, subject, topic,
-            question,
-            option_a, option_b, option_c, option_d,
-            answer, explanation
-        )
-        VALUES(?,?,?,?,?,?,?,?,?,?)
-        """, (
-            row["exam"],
-            row["subject"],
-            row["topic"],
-            row["question"],
-            row["option_a"],
-            row["option_b"],
-            row["option_c"],
-            row["option_d"],
-            row["answer"],
-            row["explanation"]
-        ))
+    if count == 0:
+        import csv
+        import os
 
-conn.commit()
-print("Questions imported successfully!")
+        csv_path = "questions_usmle.csv"
+
+        with open(csv_path, newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                cursor.execute("""
+                INSERT INTO questions(
+                    exam, subject, topic,
+                    question,
+                    option_a, option_b, option_c, option_d,
+                    answer, explanation
+                )
+                VALUES(?,?,?,?,?,?,?,?,?,?)
+                """, (
+                    row["exam"],
+                    row["subject"],
+                    row["topic"],
+                    row["question"],
+                    row["option_a"],
+                    row["option_b"],
+                    row["option_c"],
+                    row["option_d"],
+                    row["answer"],
+                    row["explanation"]
+                ))
+
+        conn.commit()
+        print("Questions loaded")
